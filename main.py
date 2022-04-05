0b537a8ba1e55ffa2c3a6f646e0ebcb08d2a1792
SubhadeepZilong <63146468+SubhadeepZilong@users.noreply.github.com>
2022-04-05 14:51:41 +0530

    updated
    Added support for Video (only audio) files

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import moviepy.editor 
 from tkinter.filedialog import *
 
+
+
 video = askopenfilename()
 video = moviepy.editor.VideoFileClip(video)
 audio=video.audio
 save_path = asksaveasfilename()
 audio.write_audiofile(save_path+".mp3")
+
+# For video (with just audio) in mp4 format use the below format and comment the previous part
+
+"""
+
+video = askopenfilename()
+video = moviepy.editor.AudioFileClip(video)
+save_path = asksaveasfilename()
+audio.write_audiofile(save_path+".mp3")
+
+"""
